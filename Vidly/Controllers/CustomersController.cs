f502b352cc5a05800824cc4fa81a57fa25d8ca43
fabio-martin <fabiomartin2006@gmail.com>
2021-07-07 10:14:12 +0100

    asp mvc with Movies and Customer Controller and their views already added

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+using Vidly.Models;
+using WebGrease.Activities;
+
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Movies
+        public ViewResult Index()
+        {
+
+            var customers = GetCustomers();
+            
+            return View(customers);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "error");
+            }
+                
+
+            return View(new Customer { Id = 1, Name = "John Smith" });
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
